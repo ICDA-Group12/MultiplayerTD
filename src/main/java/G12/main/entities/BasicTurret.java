6d59e06e6be101b89caded5df0ab479a53dec57f
ViTeXFTW <mads213@gmail.com>
2024-01-05 14:45:17 +0100

    Added template class for turrets and created an abstract type for coordinates.

---
+++ src/main/java/G12/main/entities/BasicTurret.java
@@ -0,0 +1,14 @@
+package G12.main.entities;
+
+/**
+ * A basic turret.
+ *
+ *
+ * @version 0.1.0
+ */
+public class BasicTurret extends TurretTemplate {
+
+    public BasicTurret(int x, int y, int range, int damage, int cost) {
+        super(x, y, range, damage, cost);
+    }
+}
