75977bae98f948e7b6fb27e54974436c1cc547be
ViTeXFTW <mads213@gmail.com>
2024-01-05 09:54:43 +0100

    Initial commit. Created project with Maven for JavaFX and pSpaces. Created main class and app class for running the application.

---
+++ src/main/java/G12/pSpace/App.java
@@ -0,0 +1,20 @@
+package G12.pSpace;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class for JavaFX application.
+ *
+ * @version 1.0
+ */
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+}
---
+++ src/main/java/G12/pSpace/Main.java
@@ -0,0 +1,14 @@
+package G12.pSpace;
+
+/**
+ * Main class for the pSpace project.
+ *
+ * @version 1.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println("Running pSpace application...");
+    }
+}
