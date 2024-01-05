9e2196cb2c9806ace957f724df92471b38dead52
ViTeXFTW <mads213@gmail.com>
2024-01-05 10:19:35 +0100

    Added boilerplate code to App.java, and removed Main.java as it wasn't necessary.

--- src/main/java/G12/pSpace/App.java
+++ src/main/java/G12/pSpace/App.java
@@ -1,6 +1,9 @@
 package G12.pSpace;
 
 import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -11,10 +14,29 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
+    private Pane gameRoot;
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        _initIU(stage);
     }
 
+    private void _initIU(Stage primaryStage) {
+        gameRoot = new Pane();
+        Scene scene = new Scene(gameRoot, 800, 600);
+
+        // TODO: Initialize game elements here (towers, enemies, etc.)
+
+        primaryStage.setTitle("Tower Defense Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void _addGameElement(Node element) {
+        gameRoot.getChildren().add(element);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
