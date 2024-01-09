0625e736cab5fcfa65a6cc1bdf02d05dbd142a9a
Mads Rasmussen <s215521@dtu.dk>
2024-01-09 10:34:59 +0100

    Deleted unused classes TurretTemplate.java and BalloonTemplate.java

--- src/main/java/G12/main/entities/balloons/RedBalloon.java
+++
@@ -1,9 +0,0 @@
-package G12.main.entities.balloons;
-
-public class RedBalloon extends BalloonTemplate {
-
-    public RedBalloon(int x, int y, int health, int speed) {
-        super(x, y, health, speed);
-    }
-
-}
